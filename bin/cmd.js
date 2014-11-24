4971897cb4c065733c50d1b0fbbc928de48f7246
James Kyburz <james.kyburz@gmail.com>
2014-11-24 10:30:45 +0100

    Got it working

---
+++ bin/cmd.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+var user = process.env.SAUCE_USERNAME || process.argv[2];
+var key = process.env.SAUCE_ACCESS_KEY || process.argv[3];
+var build = process.env.TRAVIS_BUILD_NUMBER || process.argv[4];
+
+var url = 'https://' + user + ':' + key + '@saucelabs.com/rest/v1/' + user + '/jobs';
+
+var request = require('hyperquest');
+var jsonstream = require('JSONStream');
+var through = require('through2');
+
+request.get(url)
+  .pipe(jsonstream.parse('*.id'))
+  .pipe(through(getJobDetail))
+  .pipe(through(deleteJob))
+;
+
+function getJobDetail(id, enc, cb) {
+  var self = this;
+  request.get(url + '/' + id).pipe(jsonstream.parse('build')).on('data', data);
+  function data(jobBuild) {
+    if (jobBuild == build) self.push(id);
+    cb();
+  }
+}
+
+function deleteJob(id, enc, cb) {
+  console.log('deleting job %s', id);
+  var opt = {headers: {'Content-Length': 0}};
+  request.delete(url + '/' + id, opt, function(err, res) {
+    if (!err && res && res.statusCode === 200) {
+      console.log('deleted job %s', id);
+    } else {
+      console.error('error deleting job %s %s', id, (err ? err : ('status:' + res.statusCode)));
+    }
+    cb();
+  });
+}
--- readme.markdown
+++ readme.markdown
@@ -1,1 +1,28 @@
-# Coming soon delete current build when re running travis build
+# remove-saucelabs-jobs-by-build
+
+remove-saucelabs-jobs-by-build
+
+Comes in handy when needing to rerun a ci build for example in [travis](https://travis-ci.org).
+
+If the travis build fails and is rerun the [saucelabs badge](https://docs.saucelabs.com/reference/status-images/) can generate
+the incorrect svg badge.
+
+This utility removes the jobs for a given build.
+
+# usage
+
+```
+usage: remove-saucelabs-jobs-by-build $SAUCE_USERNAME $SAUCE_ACCESS_KEY $TRAVIS_BUILD
+```
+
+# install
+
+With [npm](https://npmjs.org) do:
+
+```
+npm install remove-saucelabs-jobs-by-build
+```
+
+# license
+
+MIT
